9f59dca9709902c4823cd7244b0d3451c9888a92
Danny575-ui <danushp.016@gmail.com>
2024-09-14 17:48:17 +0530

    next is styling

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-let score = 0;
 let humanScore = 0;
 let compScore = 0;
-let round = 0;
+
 let humanOption;
-let dispRound;
 let compOption = 0;
+
 let arr = ["rock", "paper", "scissor"];
+
+console.log("CONNECTED!");
 
 function compInput() {
   compOption = arr[Math.floor(Math.random() * 2)];
-  document.getElementById("new").innerHTML = compOption;
-
   return;
 }
-
-console.log("CONNECTED!");
-document.getElementById("round").innerHTML = "ROUND  " + round;
 
 function clickedPaper() {
   humanOption = "paper";
@@ -31,44 +27,46 @@ function clickedRock() {
 
 function clickedScissor() {
   humanOption = "scissor";
-
   runRound();
   return;
 }
 
 function runRound() {
-  if (round <= 5) {
-    compInput();
-    if (
-      (humanOption == "rock" && compOption == "scissor") ||
-      (humanOption == "paper" && compOption == "rock") ||
-      (humanOption == "scissor" && compOption == "paper")
-    ) {
-      result = "You Won!\n";
-      choice = "You chose " + humanOption + " computer chose " + compOption;
-      humanScore = humanScore + 1;
-    } else if (
-      (humanOption == "scissor" && compOption == "rock") ||
-      (humanOption == "rock" && compOption == "paper") ||
-      (humanOption == "paper" && compOption == "scissor")
-    ) {
-      result = "You Lost!\n";
-      choice = "You chose " + humanOption + " computer chose " + compOption;
-      compScore = compScore + 1;
-    } else if (humanOption == compOption) {
-      result = "It's a Tie!";
-      choice = "You both chose " + humanOption;
-    }
+  compInput();
+  if (
+    (humanOption == "rock" && compOption == "scissor") ||
+    (humanOption == "paper" && compOption == "rock") ||
+    (humanOption == "scissor" && compOption == "paper")
+  ) {
+    result = "You Won!\n";
+    choice = "You chose " + humanOption + " computer chose " + compOption;
+    humanScore = humanScore + 1;
+  } else if (
+    (humanOption == "scissor" && compOption == "rock") ||
+    (humanOption == "rock" && compOption == "paper") ||
+    (humanOption == "paper" && compOption == "scissor")
+  ) {
+    result = "You Lost!\n";
+    choice = "You chose " + humanOption + " computer chose " + compOption;
+    compScore = compScore + 1;
+  } else if (humanOption == compOption) {
+    result = "It's a Tie!";
+    choice = "You both chose " + humanOption;
+  }
 
-    if (humanScore == 5) {
-      result = "You Won the game!";
-    } else if (compScore == 5) {
-      result = "You Lost the game!";
-    }
+  if (humanScore == 5) {
+    humanScore = 0;
+    compScore = 0;
+    window.alert("YOU WIN!!");
+  } else if (compScore == 5) {
+    humanScore = 0;
+    compScore = 0;
+
+    window.alert("YOU LOSE!!");
   }
 
   document.getElementById("choice").innerHTML = choice;
-  document.getElementById("result").innerHTML = result;
+
   document.getElementById("humanscore").innerHTML = humanScore;
   document.getElementById("compscore").innerHTML = compScore;
   return;
